f24220bc5de5f7b2e7f4971065658bfd366253ba
hectorji0110 <hectorjimenez0110@gmail.com>
2026-01-11 20:02:30 -0400

    ajuste
    public

--- ProyectoFinal/Frontend/src/components/comoFunciona.jsx
+++ ProyectoFinal/Frontend/src/components/comoFunciona.jsx
@@ -1,6 +1,3 @@
-import MapPinIcon from "../assets/material-symbols-light--map-pin-heart-rounded.svg";
-import GarageIcon from "../assets/material-symbols-light--garage-home-rounded.svg";
-import SearchIcon from "../assets/search.svg";
 /**
  * HowItWorks
  *
@@ -33,7 +30,7 @@ const HowItWorks = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 flex items-center justify-center rounded-full bg-orange-500 text-white text-3xl">
               <img
-                src={SearchIcon}
+                src="../public/assets/search.svg"
                 alt="Buscar"
                 className="w-12 h-12 object-contain"
               />
@@ -46,7 +43,7 @@ const HowItWorks = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 flex items-center justify-center rounded-full bg-orange-500 text-white text-3xl">
               <img
-                src={MapPinIcon}
+                src="../src/public/material-symbols-light--map-pin-heart-rounded.svg"
                 alt="Buscar"
                 className="w-12 h-12 object-contain"
               />
@@ -59,7 +56,7 @@ const HowItWorks = () => {
           <div className="flex flex-col items-center">
             <div className="w-16 h-16 flex items-center justify-center rounded-full bg-orange-500 text-white text-3xl">
               <img
-                src={GarageIcon}
+                src="../src/public/material-symbols-light--garage-home-rounded.svg"
                 alt="Buscar"
                 className="w-12 h-12 object-contain"
               />
